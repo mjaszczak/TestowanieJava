bce9552e685d8cc6ecf1309bcb6360a90b2c098e
mjaszczak <mjaszczak@sigma.ug.edu.pl>
2016-02-29 12:58:40 +0100

    wejsciowka commit2

--- wejsciowka01/src/main/java/com/example/BikeManager.java
+++ wejsciowka01/src/main/java/com/example/BikeManager.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import java.util.List;
+
 /**
  * Created by Mariusz on 29.02.2016.
  */
 public class BikeManager {
-
+    List<Bike> nazwa ;
 }
--- wejsciowka01/src/test/java/com/example/BikeManagerTest.java
+++ wejsciowka01/src/test/java/com/example/BikeManagerTest.java
@@ -4,4 +4,7 @@ package com.example;
  * Created by Mariusz on 29.02.2016.
  */
 public class BikeManagerTest {
+
+
+
 }
