d440bd9f6792892dd78a4000dbe320c4b62e2c65
mjaszczak <mjaszczak@sigma.ug.edu.pl>
2016-05-09 13:55:16 +0200

    dodanie usuwania

--- jdbcdemo/src/test/java/com/example/jdbcdemo/service/PersonManagerTest.java
+++ jdbcdemo/src/test/java/com/example/jdbcdemo/service/PersonManagerTest.java
@@ -14,7 +14,11 @@ public class PersonManagerTest {
 	PersonManager personManager = new PersonManager();
 	
 	private final static String NAME_1 = "Zenek";
+	private final static String NAME_2 = "Gustaw";
+	private final static String NAME_3 = "Zenobiusz";
 	private final static int YOB_1 = 1945;
+	private final static int YOB_2 = 1950;
+	private final static int YOB_3 = 1955;
 	
 	@Test
 	public void checkConnection(){
@@ -35,6 +39,21 @@ public class PersonManagerTest {
 		assertEquals(NAME_1, personRetrieved.getName());
 		assertEquals(YOB_1, personRetrieved.getYob());
 		
+	}
+
+	@Test
+	public void checkDeleting(){
+		Person person1 = new Person(NAME_1, YOB_1);
+		Person person2 = new Person(NAME_2, YOB_2);
+		Person person3 = new Person(NAME_3, YOB_3);
+		personManager.clearPersons();
+		personManager.getAllPersons();
+		assertEquals(1,personManager.addPerson(person1));
+		assertEquals(1,personManager.addPerson(person2));
+		assertEquals(1,personManager.addPerson(person3));
+
+		personManager.deletePerson(person2);
+
 	}
 
 }
